a749d093f1d30794cf9d3f7608d772002fccfc00
Maddox <roma.kolesnyk20001@gmail.com>
2020-03-31 14:25:18 +0300

    Kolisnyk. Modified file Homework1.php

--- Homework1.php
+++ Homework1.php
@@ -1,71 +1,93 @@
 <?php
 
-class Animal{
+class Animal
+{
     private $type, $class, $family, $kind;
 
-    public function getType(){
+    public function getType()
+    {
         return $this->type;
     }
 
-    public function setType($type){
-        $available_types = ['Spongia', 'Plathelmintes', 'Mollusca', 'Arthropoda', 'Chordata'];
-        if(is_string($type) && in_array($type, $available_types)){
+    public function setType($type)
+    {
+        $available_types = [
+            'Spongia', 'Plathelmintes', 'Mollusca', 'Arthropoda', 'Chordata'
+        ];
+        if (is_string($type) && in_array($type, $available_types)) {
             $this->type = $type;
             return true;
-        }else {
+        } else {
             return false;
         }
     }
 
-    public function getClass(){
+    public function getClass()
+    {
         return $this->class;
-}
+    }
 
-    public function setClass($class){
+    public function setClass($class)
+    {
         $available_classes = [
             'Crustacea', 'Arachnida', 'Insécta', 'Mammalia', 'Aves',
             'Reptilia', 'Amphibia', 'Pisces'
         ];
-        if(is_string($class) && in_array($class, $available_classes)){
+        if (is_string($class) && in_array($class, $available_classes)) {
             $this->class = $class;
             return true;
-        }else{
+        } else {
             return false;
         }
     }
 
-    public function getFamily(){
+    public function getFamily()
+    {
         return $this->family;
     }
 
-    public function setFamily($family){
-        $available_families = ['Hare', 'Squirrel', 'Mouse', 'Felidae', 'Сanine',
-            'Bear'];
-        if(is_string($family) && in_array($family, $available_families)){
+    public function setFamily($family)
+    {
+        $available_families = [
+            'Hare', 'Squirrel', 'Mouse', 'Felidae', 'Сanine', 'Bear'
+        ];
+        if (is_string($family) && in_array($family, $available_families)) {
             $this->family = $family;
             return true;
-        }else{
+        } else {
             return false;
         }
     }
 
-    public function getKind(){
+    public function getKind()
+    {
         return $this->kind;
     }
 
-    public function setKind($kind){
+    public function setKind($kind)
+    {
         $available_kind = [
-          'Rabbit', 'Dog', 'Fox', 'Cat', 'Monkey', 'Spider', 'Goat', 'Pig', 'Cow', 'Bird'
+            'Rabbit', 'Dog', 'Fox', 'Cat', 'Monkey', 'Spider', 'Goat', 'Pig', 'Cow', 'Bird'
         ];
-        if(is_string($kind) && in_array($kind, $available_kind)){
+        if (is_string($kind) && in_array($kind, $available_kind)) {
             $this->kind = $kind;
             return true;
-        }else {
+        } else {
             return false;
         }
     }
 
-    public function __construct($type, $class, $family, $kind){
+    public function getInfo()
+    {
+        return
+            "Our animal has type " . $this->getType() . ', ' .
+            "class is " . $this->getClass() . ", " .
+            'from family ' . $this->getFamily() . ", " .
+            "and it kind of " . $this->getKind() . ", ";
+    }
+
+    public function __construct($type, $class, $family, $kind)
+    {
         $this->setType($type);
         $this->setClass($class);
         $this->setFamily($family);
@@ -74,40 +96,53 @@ class Animal{
 
 }
 
-class Mammal extends Animal{
-   private $subclass;
+class Mammal extends Animal
+{
+    private $subclass;
 
-   public function getSubclass(){
-       return $this->subclass;
-   }
+    public function getSubclass()
+    {
+        return $this->subclass;
+    }
 
-    public function setSubclass($subclass){
+    public function setSubclass($subclass)
+    {
         $available_subclass = [
             "Prototheria", "Monotremata", "Theria", "Metatheria", "Eutheria"
         ];
-        if(is_string($subclass) && in_array($subclass, $available_subclass)){
+        if (is_string($subclass) && in_array($subclass, $available_subclass)) {
             $this->subclass = $subclass;
             return true;
-        }else {
+        } else {
             return false;
         }
     }
-    public function __construct($subclass){
+
+    public function getInfo()
+    {
+        return
+            "subclass of mammal is " . $this->getSubclass() . '. ';
+    }
+
+    public function __construct($subclass)
+    {
         $this->setSubclass($subclass);
     }
 }
 
-
-class Cat extends Mammal {
+class Cat extends Mammal
+{
     private $color, $nickname, $breed, $age;
 
-    public function getColor(){
+    public function getColor()
+    {
         return $this->color;
     }
 
-    public function setColor($color){
+    public function setColor($color)
+    {
         $colors_available = ['grey', 'black', 'white', 'brown'];
-        if(!empty($color) && is_string($color) && in_array($color, $colors_available)) {
+        if (!empty($color) && is_string($color) && in_array($color, $colors_available)) {
             $this->color = $color;
             return true;
         } else {
@@ -115,58 +150,62 @@ class Cat extends Mammal {
         }
     }
 
-    public function getNickname(){
+    public function getNickname()
+    {
         return $this->nickname;
     }
 
-    public function setNickname($nickname){
+    public function setNickname($nickname)
+    {
         if (is_string($nickname)) {
             $this->nickname = $nickname;
             return true;
-        }else{
+        } else {
             return false;
         }
     }
 
-    public function getAge(){
+    public function getAge()
+    {
         return $this->age;
     }
 
-    public function setAge($age){
+    public function setAge($age)
+    {
         if (is_numeric($age)) {
             $this->age = $age;
             return true;
-        }else{
+        } else {
             return false;
         }
     }
-    public function getBreed(){
+
+    public function getBreed()
+    {
         return $this->breed;
     }
 
-    public function setBreed($breed){
-        if(is_string($breed)){
+    public function setBreed($breed)
+    {
+        if (is_string($breed)) {
             $this->breed = $breed;
             return true;
-        }else{
+        } else {
             return false;
         }
     }
 
-    public function getInfo(){
+    public function getInfo()
+    {
         return
-            "Our animal has type ". $this->getType().', '.
-            "class is". $this->getClass().", ".
-            "subclass of mammal is ".$this->getSubclass().', '.
-            'from family'. $this->getFamily().", ".
-            "and it has kind of ". $this->getKind().". ".
-            "The name of cat is " . $this->getNickname(). ', '.
-            'it is '.$this->getAge() .' years old, '.
-            'color is '.$this->getColor(). ', '.
-            'breed is '.$this->getBreed().'.';
+            "The name of cat is " . $this->getNickname() . ', ' .
+            'it is ' . $this->getAge() . ' years old, ' .
+            'color is ' . $this->getColor() . ', ' .
+            'breed is ' . $this->getBreed() . '. ';
     }
 
-    public function __construct($nickname, $color, $breed, $age){
+    public function __construct($nickname, $color, $breed, $age)
+    {
         $this->setNickname($nickname);
         $this->setColor($color);
         $this->setBreed($breed);
@@ -175,15 +214,10 @@ class Cat extends Mammal {
 
 }
 
-
-
-
 $animal = new Animal("Chordata", "Mammalia", "Felidae", "Cat");
 $mammal = new Mammal("Theria");
 $murzik = new Cat("Murzik", "brown", "Abyssinian", 5);
-echo $murzik->getInfo();
 
-
-
+echo $animal->getInfo() . $mammal->getInfo() . $murzik->getInfo();
 
 ?>
